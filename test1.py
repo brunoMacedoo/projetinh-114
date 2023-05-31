5c3a03e89f864feaecbb8c3d6f2912bb58790c9b
brunoMacedoo <102264136+brunoMacedoo@users.noreply.github.com>
2023-05-31 19:20:30 -0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+a =  33
+b =  int("33")
+if b>a:
+    print("b é maior que a")
+elif b == a:
+    print ("a e b são iguais")
+
+#não se pode colocar um maior que (>) entre uma string e uma inteira
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + " refsnes")
+
+my_function("Emilly")
+my_function("Tobias")
+my_function("Lucas")
